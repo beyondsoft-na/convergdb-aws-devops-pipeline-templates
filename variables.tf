cd997717374b6c5df0f6721d5f9a302681a1de5d
Rui Pan <ruipan@BCI-Rui-MacBook-Pro.attlocal.net>
2020-07-23 14:00:07 -0500

    terraform init test

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "deployment_id" {}
+
+variable "region" {}
+
+variable "admin_bucket" {}
+
+variable "data_bucket" {}
+
+variable "etl_lock_table" {}
+
+variable "fargate_cluster" {}
+
+variable "fargate_vpc" {}
+
+variable "fargate_subnet" {}
+
+variable "ecs_log_group" {}
+
+variable "ecs_execution_role" {}
+
